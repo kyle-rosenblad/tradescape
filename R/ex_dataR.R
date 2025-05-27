2393fed3cd83161c186e9e4f8bee442a5ccca394
Kyle Rosenblad <kyle.c.rosenblad@gmail.com>
2025-05-26 20:49:51 -0700

    documented example data

---
+++ R/ex_dataR.R
@@ -0,0 +1,11 @@
+#' A simulated landscape genomic data set
+#'
+#'
+#' List containing:
+#' allele_count, an integer vector of counts of the reference allele for simulated individuals
+#' snp_idx, an integer vector indicating which SNP (i.e., point in the genome) each allele count pertains to
+#' site_idx, an integer vector indicating which geographic sampling site each allele count pertaints to
+#' envdata, a two-column data frame, in which rows are individuals (not sites), and columns are environmental variables describing the conditions at the site where each individual was sampled
+#' coords, a data frame in which rows represent geographic sites and the two columns are x and y coordinates
+#'
+"ex_data"

cfbf9c487cc7615a346630f01b4b6ef82ded9beb
Kyle Rosenblad <kyle.c.rosenblad@gmail.com>
2025-05-26 20:18:45 -0700

    removed deprecated doctype roxygen tag

--- R/tradescape-package.R
+++ R/tradescape-package.R
@@ -2,7 +2,7 @@
 #'
 #' @description Hierarchical Bayesian modeling of local adaptation to multidimensional environmental gradients with SNP data
 #'
-#' @docType _PACKAGE
+#' _PACKAGE
 #' @name tradescape-package
 #' @aliases tradescape
 #' @useDynLib tradescape, .registration = TRUE
